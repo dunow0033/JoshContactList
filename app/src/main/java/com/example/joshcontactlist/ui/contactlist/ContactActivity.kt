9a08c0e8d224926f7cd42bded792b576804c2f05
Brian Donnoe <dunow33@hotmail.com>
2021-11-01 16:28:32 -0400

    initial setup

---
+++ app/src/main/java/com/example/joshcontactlist/ui/contactlist/ContactActivity.kt
@@ -0,0 +1,12 @@
+package com.example.joshcontactlist.ui.contactlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.joshcontactlist.R
+
+class ContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+}
